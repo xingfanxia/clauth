d7e083218980bd8104da009efc5e8f33a69dc67b
cloudy <37777261+uwuclxdy@users.noreply.github.com>
2026-04-28 17:42:06 +0200

    feat: quit after switching profiles

--- src/menu.rs
+++ src/menu.rs
@@ -48,7 +48,10 @@ pub(crate) fn profile_submenu(config: &mut AppConfig, profile_name: &str) -> Res
         };
 
         let result: Result<bool> = match ACTIONS[idx] {
-            Switch => switch_profile(config, profile_name).map(|_| true),
+            Switch => {
+                switch_profile(config, profile_name)?;
+                std::process::exit(0);
+            }
             Edit   => edit_profile(config, profile_name).map(|_| false),
             Rename => rename_profile(config, profile_name),
             Delete => delete_profile(config, profile_name),
